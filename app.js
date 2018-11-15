a2440ff1e01efe9596cb47deb635c0da352da962
Avaloja16 <aleksi.valojaa@edu.turkuamk.fi>
2018-11-15 19:38:07 +0200

    Added routers and controllers

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var createError = require('http-errors');
 var express = require('express');
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://Avaloja16:nodejs1@ds125680.mlab.com:25680/local_library111';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+
+var wiki = require('./wiki.js');
 
 var app = express();
 
@@ -19,8 +31,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+
+app.use('/wiki', wiki);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
